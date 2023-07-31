dd99f39999fa632a91215f5c0605a0559930b270
pelmeshki1 <108807142+pelmeshki1@users.noreply.github.com>
2023-07-31 22:14:15 +0300

    update 4

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+import throttle from 'lodash.throttle';
 
+const inputEl = document.querySelector('input');
+const messageEl = document.querySelector('textarea');
+const form = document.querySelector('.feedback-form');
+const FORM_STATE = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
