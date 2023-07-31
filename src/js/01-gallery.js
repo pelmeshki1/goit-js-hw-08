150fd74ed18ff3cf47457d2fb47f257a724ad850
pelmeshki1 <108807142+pelmeshki1@users.noreply.github.com>
2023-07-31 21:00:38 +0300

    update 2.2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,10 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const makeGalleryCard = ({ preview, original, description }) =>
+  `<a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
