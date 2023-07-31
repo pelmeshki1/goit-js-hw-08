4bb6b981c41e84fe03fcebde282929dbd6cee680
pelmeshki1 <108807142+pelmeshki1@users.noreply.github.com>
2023-07-31 22:26:10 +0300

    update 7.1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,5 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const CURRENT_TIME = 'videoplayer-current-time';
